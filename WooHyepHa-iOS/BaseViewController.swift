652e30554940803c564c146242d33a2555721c95
YeoSeongil <seongil_yeo@naver.com>
2024-07-31 17:12:09 +0900

    [FEAT] #1 - BaseViewController 구현

--- WooHyepHa-iOS/BaseViewController.swift
+++ WooHyepHa-iOS/BaseViewController.swift
@@ -7,23 +7,41 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+import RxSwift
 
+class BaseViewController: UIViewController {
+    
+    final let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setViewController()
+        setConstraints()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    */
-
+    
+    // MARK: setUp ViewController Method
+        /// ViewController의 기초를 설정합니다. (배경색상 등)
+        /// ```
+        /// func setViewController() {
+        ///    self.view.backgroundColor = .white
+        ///    // another codes ...
+        /// }
+        /// ```
+        func setViewController() { }
+    
+        /// UI 프로퍼티의 제약조건을 설정합니다.
+        /// (본 프로젝트에서는 Snapkit을 사용합니다.)
+        /// ```
+        /// func setConstraints() {
+        ///    view.snp.makeConstraints {
+        ///       $0.width.equalTo(10)
+        ///    // another codes ...
+        /// }
+        /// ```
+        func setConstraints() { }
 }
