210f5ae854cc2e997d76ef23a96323f3961569b2
YeoSeongil <seongil_yeo@naver.com>
2024-08-13 12:37:53 +0900

    [FEAT] #3 - MyPageCoordinator 구현

--- WooHyepHa-iOS/Sources/Presentation/Tab/MyPage/MyPageCoordinator.swift
+++ WooHyepHa-iOS/Sources/Presentation/Tab/MyPage/MyPageCoordinator.swift
@@ -5,4 +5,27 @@
 //  Created by 여성일 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+final class MyPageCoordinator: Coordinator {
+   weak  var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        goToMyPageViewController()
+    }
+}
+
+private extension MyPageCoordinator {
+    func goToMyPageViewController() {
+        let mypageViewController = MyPageViewController()
+        navigationController.pushViewController(mypageViewController, animated: true)
+    }
+}
