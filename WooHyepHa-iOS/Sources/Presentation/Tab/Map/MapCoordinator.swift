7bb43b10114d98910ab0700426eef3befdf54d19
YeoSeongil <seongil_yeo@naver.com>
2024-08-13 12:33:17 +0900

    [FEAT] #3 - MapCoordinator 구현

--- WooHyepHa-iOS/Sources/Presentation/Tab/Map/MapCoordinator.swift
+++ WooHyepHa-iOS/Sources/Presentation/Tab/Map/MapCoordinator.swift
@@ -5,4 +5,27 @@
 //  Created by 여성일 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+final class MapCoordinator: Coordinator {
+   weak  var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        goToMapViewController()
+    }
+}
+
+private extension MapCoordinator {
+    func goToMapViewController() {
+        let MapViewController = MapViewController()
+        navigationController.pushViewController(MapViewController, animated: true)
+    }
+}
