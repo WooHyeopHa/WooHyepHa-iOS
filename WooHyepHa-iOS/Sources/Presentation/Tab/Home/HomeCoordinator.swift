3aae1ac2e33ab132792b9b315dc48fec00823429
YeoSeongil <seongil_yeo@naver.com>
2024-08-13 12:29:45 +0900

    [FEAT] #3 - HomeCoordinator 구현

--- WooHyepHa-iOS/Sources/Presentation/Tab/Home/HomeCoordinator.swift
+++ WooHyepHa-iOS/Sources/Presentation/Tab/Home/HomeCoordinator.swift
@@ -5,4 +5,28 @@
 //  Created by 여성일 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+final class HomeCoordinator: Coordinator {
+   weak  var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        goToHomeViewController()
+    }
+}
+
+private extension HomeCoordinator {
+    func goToHomeViewController() {
+        let homeViewController = HomeViewController()
+        navigationController.pushViewController(homeViewController, animated: true)
+    }
+}
+
