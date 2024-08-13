8f20741fff658cbe8fa51677fbf93fcbb20af243
YeoSeongil <seongil_yeo@naver.com>
2024-08-13 12:35:02 +0900

    [FEAT] #3 - ChatCoordinator 구현

--- WooHyepHa-iOS/Sources/Presentation/Tab/Chat/ChatCoordinator.swift
+++ WooHyepHa-iOS/Sources/Presentation/Tab/Chat/ChatCoordinator.swift
@@ -5,4 +5,27 @@
 //  Created by 여성일 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+final class ChatCoordinator: Coordinator {
+   weak  var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        goToChatViewController()
+    }
+}
+
+private extension ChatCoordinator {
+    func goToChatViewController() {
+        let chatViewController = ChatViewController()
+        navigationController.pushViewController(chatViewController, animated: true)
+    }
+}
