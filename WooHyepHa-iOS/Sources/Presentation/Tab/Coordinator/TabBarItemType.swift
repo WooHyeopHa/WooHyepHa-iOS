5143ccde32eab41102fa651d1c34d39d016147c8
YeoSeongil <seongil_yeo@naver.com>
2024-08-13 12:21:52 +0900

    [FEAT] #3 - TabBarItemType 구현

--- WooHyepHa-iOS/Sources/Presentation/Tab/Coordinator/TabBarItemType.swift
+++ WooHyepHa-iOS/Sources/Presentation/Tab/Coordinator/TabBarItemType.swift
@@ -5,4 +5,25 @@
 //  Created by 여성일 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+enum TabBarItemType: Int, CaseIterable {
+    case home = 0, mating, map, chat, mypage
+}
+
+extension TabBarItemType {
+    var tabBarItem: UITabBarItem {
+        switch self {
+        case .home:
+            return UITabBarItem(title: "", image: UIImage(named: "tabbar_home_inactive")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "tabbar_home_active")?.withRenderingMode(.alwaysOriginal))
+        case .mating:
+            return UITabBarItem(title: "", image: UIImage(named: "tabbar_mating_inactive")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "tabbar_mating_active")?.withRenderingMode(.alwaysOriginal))
+        case .map:
+            return UITabBarItem(title: "", image: UIImage(named: "tabbar_map_inactive")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "tabbar_map_active")?.withRenderingMode(.alwaysOriginal))
+        case .chat:
+            return UITabBarItem(title: "", image: UIImage(named: "test1")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "test2")?.withRenderingMode(.alwaysOriginal))
+        case .mypage:
+            return UITabBarItem(title: "", image: UIImage(named: "tabbar_mypage_inactive")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "tabbar_mypage_active")?.withRenderingMode(.alwaysOriginal))
+        }
+    }
+}
