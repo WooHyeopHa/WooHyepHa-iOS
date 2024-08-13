d9a4a8304c0bdcee09abb1bb7d17d33615fb4029
YeoSeongil <seongil_yeo@naver.com>
2024-08-13 12:24:59 +0900

    [FEAT] #3 - Coordinator 구현

--- WooHyepHa-iOS/Sources/App/Coordinator/Coordinator.swift
+++ WooHyepHa-iOS/Sources/App/Coordinator/Coordinator.swift
@@ -5,4 +5,23 @@
 //  Created by 여성일 on 8/13/24.
 //
 
-import Foundation
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var parentCoordinator: Coordinator? { get set }
+    var children: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    
+    func start()
+}
+
+extension Coordinator {
+    func childDidFinish(_ coordinator : Coordinator){
+        for (index, child) in children.enumerated() {
+            if child === coordinator {
+                children.remove(at: index)
+                break
+            }
+        }
+    }
+}
