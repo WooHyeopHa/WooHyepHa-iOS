5038e3eb8581acdff0def63b1f22e2aed52ad12e
YeoSeongil <seongil_yeo@naver.com>
2024-08-14 14:04:32 +0900

    [FEAT] #7 - BaseHeaderView 구현

--- WooHyepHa-iOS/Sources/Utils/Base/BaseHeaderView.swift
+++ WooHyepHa-iOS/Sources/Utils/Base/BaseHeaderView.swift
@@ -7,14 +7,58 @@
 
 import UIKit
 
+import RxSwift
+import Then
+
 class BaseHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    final let disposeBag = DisposeBag()
+    
+    var showBottomeBorder: Bool = true {
+        didSet {
+            bottomBorder.isHidden = !showBottomeBorder
+        }
     }
-    */
+    
+    private let bottomBorder = CALayer().then {
+        $0.backgroundColor = UIColor.lightGray.cgColor
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setHeaderView()
+        setConstraints()
+        layer.addSublayer(bottomBorder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+           super.layoutSubviews()
+           bottomBorder.frame = CGRect(x: 0, y: frame.height - 1, width: frame.width, height: 1)
+       }
+    
+    // MARK: SetUp HeaderView Method
+        /// HeaderView의 기초를 설정합니다. (배경색상 등)
+        /// ```
+        /// func setHeaderView() {
+        ///    backgroundColor = .white
+        ///    // another codes ...
+        /// }
+        /// ```
+        func setHeaderView() { }
+    
+        /// UI 프로퍼티의 제약조건을 설정합니다.
+        /// (본 프로젝트에서는 Snapkit을 사용합니다.)
+        /// ```
+        /// func setConstraints() {
+        ///    button.snp.makeConstraints {
+        ///       $0.width.equalTo(10)
+        ///    // another codes ...
+        /// }
+        /// ```
+        func setConstraints() { }
 
 }
