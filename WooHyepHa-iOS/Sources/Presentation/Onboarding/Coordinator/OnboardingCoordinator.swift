466e6a0751965e03934f4af55e375cad6553ddfd
YeoSeongil <seongil_yeo@naver.com>
2024-08-14 14:03:49 +0900

    [FEAT] #7 - goToLoginViewController() 구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/Coordinator/OnboardingCoordinator.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/Coordinator/OnboardingCoordinator.swift
@@ -5,4 +5,29 @@
 //  Created by 여성일 on 8/14/24.
 //
 
-import Foundation
+import UIKit
+
+final class OnboardingCoordinator: Coordinator {
+    weak var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController){
+        self.navigationController = navigationController
+        navigationController.navigationBar.isHidden = true
+    }
+    
+    func start() {
+        goToLoginViewController()
+    }
+}
+
+extension OnboardingCoordinator {
+    func goToLoginViewController() {
+        let loginViewController = LoginViewController()
+        loginViewController.coordinator = self
+        navigationController.pushViewController(loginViewController, animated: true)
+    }
+}
