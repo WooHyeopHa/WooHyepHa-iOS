701475ad4fe9e7d6ebfb7ccb0fd9f0b38358f7d8
YeoSeongil <seongil_yeo@naver.com>
2024-08-14 12:59:04 +0900

    [ADD] #7 - BaseHeaderView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Utils/Base/BaseHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseHeaderView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/14/24.
+//
+
+import UIKit
+
+class BaseHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
