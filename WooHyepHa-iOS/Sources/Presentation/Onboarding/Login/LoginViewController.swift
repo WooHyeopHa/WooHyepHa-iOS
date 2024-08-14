4537b0915993af3c94aafebd650b72f636e9aec9
YeoSeongil <seongil_yeo@naver.com>
2024-08-14 14:03:12 +0900

    [FEAT] #7 - 로그인 뷰컨트롤러 구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/Login/LoginViewController.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/Login/LoginViewController.swift
@@ -7,23 +7,42 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: BaseViewController {
 
+    // 나중에 로그인 기능 구현 시 코디네이터는 뷰모델로 옮겨 주입 받을 예정
+    weak var coordinator: OnboardingCoordinator?
+    
+    // MARK: UI Components
+    private lazy var headerView = LoginHeaderView().then {
+        $0.delegate = self
+        $0.showBottomeBorder = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Set ViewController
+    override func setViewController() {
+        view.backgroundColor = .black
+        
+        [headerView].forEach {
+            view.addSubview($0)
+        }
     }
-    */
+    
+    override func setConstraints() {
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(56)
+        }
+    }
+}
 
+extension LoginViewController: LoginHeaderViewDelegate {
+    func leftButtonDidTap() {
+        print("testLog : ButtonTapped")
+    }
 }
