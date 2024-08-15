b28b8e73d63365e196ff8d68dc03c2372f9777fc
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 16:14:10 +0900

    [FEAT] #12 - RegisterProfileFooterView UI구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterProfile/Components/RegisterProfileFooterView.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterProfile/Components/RegisterProfileFooterView.swift
@@ -7,14 +7,51 @@
 
 import UIKit
 
-class RegisterProfileFooterView: UIView {
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+protocol RegisterProfileFooterViewDelegate: AnyObject {
+    func nextButtonDidTap()
+}
+
+class RegisterProfileFooterView: BaseView {
+
+    weak var delegate: RegisterProfileFooterViewDelegate?
+    
+    // MARK: UI Components
+    private let nextButton = UIButton().then {
+        $0.setTitle("다음", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .body2
+        $0.backgroundColor = .gray1
+        $0.layer.cornerRadius = 10
     }
-    */
-
+    
+    // MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: SetUp View
+    override func setView() {
+        backgroundColor = .white
+        [nextButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(48)
+        }
+    }
 }
