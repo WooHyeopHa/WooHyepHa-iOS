bac6c9d4b77b8600937b44755b449113c2ad2c6a
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 15:58:36 +0900

    [ADD] #12 - RegisterProfileFooterView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterProfile/Components/RegisterProfileFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterProfileFooterView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/15/24.
+//
+
+import UIKit
+
+class RegisterProfileFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
