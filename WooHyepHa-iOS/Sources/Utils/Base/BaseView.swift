303d3fc3edec6fff329809748caa131b27a0888e
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 14:24:59 +0900

    [FEAT] #12 - BaseView 구현

--- WooHyepHa-iOS/Sources/Utils/Base/BaseView.swift
+++ WooHyepHa-iOS/Sources/Utils/Base/BaseView.swift
@@ -7,14 +7,42 @@
 
 import UIKit
 
+import RxSwift
+import Then
+
 class BaseView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    final let disposeBag = DisposeBag()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setView()
+        setConstraints()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: SetUp View Method
+        /// View의 기초를 설정합니다. (배경색상 등)
+        /// ```
+        /// func setView() {
+        ///    backgroundColor = .white
+        ///    // another codes ...
+        /// }
+        /// ```
+        func setView() { }
+    
+        /// UI 프로퍼티의 제약조건을 설정합니다.
+        /// (본 프로젝트에서는 Snapkit을 사용합니다.)
+        /// ```
+        /// func setConstraints() {
+        ///    button.snp.makeConstraints {
+        ///       $0.width.equalTo(10)
+        ///    // another codes ...
+        /// }
+        /// ```
+        func setConstraints() { }
 
 }
