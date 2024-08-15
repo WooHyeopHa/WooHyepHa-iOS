fd124f1671a19bb0a0432f5f561a99dd995dc717
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 14:24:43 +0900

    [ADD] #12 - BaseView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Utils/Base/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/15/24.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
