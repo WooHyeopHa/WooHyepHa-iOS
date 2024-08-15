346764a05b890ab611dbd0bb047424f0409afb56
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 12:14:24 +0900

    [FEAT] #5 - 페이드 인, 아웃 메소드 구현하여 extension으로 확장

--- WooHyepHa-iOS/Sources/Utils/Extensions/Extension + UIView.swift
+++ WooHyepHa-iOS/Sources/Utils/Extensions/Extension + UIView.swift
@@ -5,4 +5,18 @@
 //  Created by 여성일 on 8/15/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func fadeIn(duration: TimeInterval) {
+        UIView.animate(withDuration: duration) {
+            self.alpha = 1.0
+        }
+    }
+    
+    func fadeOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration) {
+            self.alpha = 0.0
+        }
+    }
+}
