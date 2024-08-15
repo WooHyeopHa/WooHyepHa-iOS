4bb441004e8788c2a36a73d5cbecaf2fe5b77ada
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 13:24:31 +0900

    [FEAT] #12 - 가입축하&프로필 등록 RegisterViewController 구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/Register/RegisterViewController.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/Register/RegisterViewController.swift
@@ -7,23 +7,86 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class RegisterViewController: BaseViewController {
 
-        // Do any additional setup after loading the view.
+    weak var coordinator: OnboardingCoordinator?
+    
+    //MARK: UI Components
+    private let mainTitleLabel = UILabel().then {
+        $0.text = "가입을 축하드립니다!"
+        $0.textColor = .gray7
+        $0.font = .h2
+        $0.textAlignment = .center
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let subTitleLabel = UILabel().then {
+        $0.text = "Let's find muse!"
+        $0.textColor = .white
+        $0.font = .body2
+        $0.textAlignment = .center
     }
-    */
+    
+    private let registerButton = UIButton().then {
+        $0.setTitle("프로필 등록하기", for: .normal)
+        $0.setTitleColor(.gray2, for: .normal)
+        $0.titleLabel?.font = .body2
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 5
+    }
+    
+    private let nextRegisterButton = UIButton().then {
+        let attributedString = NSMutableAttributedString(string: "다음에 등록할게요!", attributes: [.font: UIFont.body4, .foregroundColor: UIColor.gray8])
+        attributedString.append(NSAttributedString(string: "   Skip", attributes: [.font: UIFont.body4, .foregroundColor: UIColor.MainColor]))
+        $0.setAttributedTitle(attributedString, for: .normal)
+        $0.titleLabel?.numberOfLines = 2
+        $0.backgroundColor = .clear
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Set ViewController
+    override func setViewController() {
+        view.backgroundColor = .black
+        
+        [mainTitleLabel, subTitleLabel, registerButton, nextRegisterButton].forEach {
+            view.addSubview($0)
+        }
+        
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func setConstraints() {
+        mainTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(252)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(48)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(mainTitleLabel.snp.bottom)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(40)
+        }
+        
+        registerButton.snp.makeConstraints {
+            $0.bottom.equalTo(nextRegisterButton.snp.top)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(48)
+        }
+        
+        nextRegisterButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(60)
+        }
+    }
 
 }
