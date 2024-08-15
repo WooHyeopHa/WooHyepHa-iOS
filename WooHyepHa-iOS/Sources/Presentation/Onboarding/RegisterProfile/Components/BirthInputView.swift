6091c4d50f810e70c34a2d820c1344860a997c75
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 16:13:22 +0900

    [FEAT] #12 - BirthInputView 구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterProfile/Components/BirthInputView.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterProfile/Components/BirthInputView.swift
@@ -7,14 +7,70 @@
 
 import UIKit
 
-class BirthInputView: UIView {
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class BirthInputView: BaseView {
+
+    // MARK: UI Components
+    private let birthLabel = UILabel().then {
+        $0.text = "출생연도"
+        $0.font = .body1
+        $0.textColor = .gray1
     }
-    */
-
+    
+    private let birthTextField = UITextField().then {
+        $0.placeholder = "태어난 연도를 설정해 주세요!"
+        $0.font = .body4
+        $0.layer.borderColor = UIColor.gray7.cgColor
+        $0.layer.borderWidth = 1
+        $0.layer.cornerRadius = 10
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 0))
+        $0.leftViewMode = .always
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func setView() {
+        backgroundColor = .white
+        
+        [birthLabel, birthTextField].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        birthLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+            $0.height.equalTo(42)
+        }
+        
+        birthTextField.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(48)
+        }
+    }
+    
+    //MARK: Bind
+    override func bind() {
+    }
 }
+
+// MARK: View Method
+extension BirthInputView {
+}
+
+//MARK: Observable
+extension BirthInputView {
+}
+
