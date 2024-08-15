a96366bf70332bbccdc6eb4dfaf6c8c49e8ee2b7
YeoSeongil <seongil_yeo@naver.com>
2024-08-15 14:53:56 +0900

    [FEAT] #12 - BaseView 바인드 메소드 추가

--- WooHyepHa-iOS/Sources/Utils/Base/BaseView.swift
+++ WooHyepHa-iOS/Sources/Utils/Base/BaseView.swift
@@ -11,13 +11,14 @@ import RxSwift
 import Then
 
 class BaseView: UIView {
-
+    
     final let disposeBag = DisposeBag()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setView()
         setConstraints()
+        bind()
     }
     
     required init?(coder: NSCoder) {
@@ -25,24 +26,25 @@ class BaseView: UIView {
     }
     
     // MARK: SetUp View Method
-        /// View의 기초를 설정합니다. (배경색상 등)
-        /// ```
-        /// func setView() {
-        ///    backgroundColor = .white
-        ///    // another codes ...
-        /// }
-        /// ```
-        func setView() { }
+    /// View의 기초를 설정합니다. (배경색상 등)
+    /// ```
+    /// func setView() {
+    ///    backgroundColor = .white
+    ///    // another codes ...
+    /// }
+    /// ```
+    func setView() { }
     
-        /// UI 프로퍼티의 제약조건을 설정합니다.
-        /// (본 프로젝트에서는 Snapkit을 사용합니다.)
-        /// ```
-        /// func setConstraints() {
-        ///    button.snp.makeConstraints {
-        ///       $0.width.equalTo(10)
-        ///    // another codes ...
-        /// }
-        /// ```
-        func setConstraints() { }
-
+    /// UI 프로퍼티의 제약조건을 설정합니다.
+    /// (본 프로젝트에서는 Snapkit을 사용합니다.)
+    /// ```
+    /// func setConstraints() {
+    ///    button.snp.makeConstraints {
+    ///       $0.width.equalTo(10)
+    ///    // another codes ...
+    /// }
+    /// ```
+    func setConstraints() { }
+    
+    func bind() { }
 }
