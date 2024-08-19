75f67303bbae75ee51715eae0dd9f0bca3344697
YeoSeongil <seongil_yeo@naver.com>
2024-08-19 18:12:29 +0900

    [FEAT] #12 - RegisterLocationFooterView 구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterLocation/Components/RegisterLocationFooterView.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterLocation/Components/RegisterLocationFooterView.swift
@@ -7,14 +7,67 @@
 
 import UIKit
 
-class RegisterLocationFooterView: UIView {
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+protocol RegisterLocationFooterViewDelegate: AnyObject {
+    func nextButtonDidTap()
+}
+
+class RegisterLocationFooterView: BaseView {
+
+    weak var delegate: RegisterLocationFooterViewDelegate?
+    
+    // MARK: UI Components
+    private let nextButton = UIButton().then {
+        $0.setTitle("다음", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = .body2
+        $0.backgroundColor = .gray6
+        $0.layer.cornerRadius = 10
+        $0.isEnabled = false
     }
-    */
+    
+    // MARK: init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: SetUp View
+    override func setView() {
+        backgroundColor = .white
+        [nextButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(48)
+        }
+    }
+    
+    override func bind() {
+        nextButton.rx.tap
+            .subscribe(with: self, onNext: { owner, _ in
+                owner.delegate?.nextButtonDidTap()
+            })
+            .disposed(by: disposeBag)
+    }
+}
 
+// MARK: View Method
+extension RegisterLocationFooterView {
+    func updateNextButtonState(isEnabled: Bool) {
+        nextButton.isEnabled = isEnabled
+        nextButton.backgroundColor = isEnabled ? .gray1 : .gray6
+    }
 }
