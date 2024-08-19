d4c573e82ee2390a0d5da2f7fa93fee71294cf9d
YeoSeongil <seongil_yeo@naver.com>
2024-08-19 16:54:44 +0900

    [ADD] #12 - RegisterLocationFooterView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterLocation/Components/RegisterLocationFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterLocationFooterView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/19/24.
+//
+
+import UIKit
+
+class RegisterLocationFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
