a042c63f9fb20dc3d3daf419c52c33fccdd7db16
YeoSeongil <seongil_yeo@naver.com>
2024-08-19 16:00:29 +0900

    [ADD] #12 - RegisterLocationViewController 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterLocation/RegisterLocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterLocationViewController.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/19/24.
+//
+
+import UIKit
+
+class RegisterLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
