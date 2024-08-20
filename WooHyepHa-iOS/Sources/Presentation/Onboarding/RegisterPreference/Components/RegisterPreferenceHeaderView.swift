7d6dbcfe887e12c507e6e1c0e1d4e302dec63821
YeoSeongil <seongil_yeo@naver.com>
2024-08-20 10:01:10 +0900

    [ADD] #12 - RegisterPreferenceHeaderView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterPreference/Components/RegisterPreferenceHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterPreferenceHeaderView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/20/24.
+//
+
+import UIKit
+
+class RegisterPreferenceHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
