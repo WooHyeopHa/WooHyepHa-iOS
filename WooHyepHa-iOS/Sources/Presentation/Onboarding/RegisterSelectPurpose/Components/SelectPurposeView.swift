f66280e0807d94b6cc604312841ab114e5e54f00
YeoSeongil <seongil_yeo@naver.com>
2024-08-21 22:37:02 +0900

    [FEAT] #12 - 관람목적 선택 페이지 구현

--- WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterSelectPurpose/Components/SelectPurposeView.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterSelectPurpose/Components/SelectPurposeView.swift
@@ -7,14 +7,110 @@
 
 import UIKit
 
-class SelectPurposeView: UIView {
+import Then
+import SnapKit
+import RxCocoa
+import RxSwift
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class SelectPurposeView: BaseView {
+    private var currentlySelectedButton: OnboardingButton?
+    
+    private let learnButton = OnboardingButton(title: "지식습득")
+    private let healingButton = OnboardingButton(title: "휴식 및 힐링")
+    private let socialActivitiesButton = OnboardingButton(title: "사회적 활동")
+    private let inspirationButton = OnboardingButton(title: "영감 및 창의성")
+    private let etcButton = OnboardingButton(title: "기타")
+    
+    private let verticalStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+        $0.spacing = 18
     }
-    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setView() {
+        showTopBorder = false
+        showBottomBorder = false
+        
+        [learnButton, healingButton, socialActivitiesButton, inspirationButton, etcButton].forEach {
+            verticalStackView.addArrangedSubview($0)
+        }
+        
+        addSubview(verticalStackView)
+    }
+    
+    override func setConstraints() {
+        verticalStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalToSuperview()
+        }
+        
+        [learnButton, healingButton, socialActivitiesButton, inspirationButton, etcButton].forEach {
+            $0.snp.makeConstraints {
+                $0.height.equalTo(48)
+            }
+        }
+    }
+    
+    override func bind() {
+        inputSelectPurpose
+            .subscribe(with: self, onNext: { owner, type in
+                owner.toggleButton(type)
+            })
+            .disposed(by: disposeBag)
+    }
+}
 
+extension SelectPurposeView {
+    enum SelectPurposeButtonType: String {
+        case learn = "learn"
+        case healing = "healing"
+        case socialActivities = "socialActivities"
+        case inspiration = "inspiration"
+        case etc = "etc"
+    }
+    
+    var inputSelectPurpose: Observable<String> {
+        return Observable.merge(
+            learnButton.rx.tap.map { SelectPurposeButtonType.learn.rawValue },
+            healingButton.rx.tap.map { SelectPurposeButtonType.healing.rawValue },
+            socialActivitiesButton.rx.tap.map { SelectPurposeButtonType.socialActivities.rawValue },
+            inspirationButton.rx.tap.map { SelectPurposeButtonType.inspiration.rawValue },
+            etcButton.rx.tap.map { SelectPurposeButtonType.etc.rawValue }
+        )
+    }
+}
+
+extension SelectPurposeView {
+    private func toggleButton(_ field: String) {
+        if let buttonType = SelectPurposeButtonType(rawValue: field) {
+            let button: OnboardingButton
+            switch buttonType {
+            case .learn:
+                button = learnButton
+            case .healing:
+                button = healingButton
+            case .socialActivities:
+                button = socialActivitiesButton
+            case .inspiration:
+                button = inspirationButton
+            case .etc:
+                button = etcButton
+            }
+            
+            if button != currentlySelectedButton {
+                currentlySelectedButton?.isSelected = false
+                button.isSelected = true
+                currentlySelectedButton = button
+            }
+        }
+    }
 }
--- WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterSelectPurpose/RegisterSelectPurposeViewController.swift
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterSelectPurpose/RegisterSelectPurposeViewController.swift
@@ -7,23 +7,119 @@
 
 import UIKit
 
-class RegisterSelectPurposeViewController: UIViewController {
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class RegisterSelectPurposeViewController: BaseViewController {
 
-        // Do any additional setup after loading the view.
+    weak var coordinator: OnboardingCoordinator?
+
+    //MARK: UI Components
+    private lazy var headerView = OnboardingHeaderView().then {
+        $0.delegate = self
+        $0.backgroundColor = .white
+        $0.rightButtonTitle = "건너뛰기"
+        $0.rightButtonTitleColor = .gray4
+    }
+
+    private let progressView = OnboardingProgressView(progressValue: 1.0)
+    
+    private let mainTitle = UILabel().then {
+        let attributedString = NSMutableAttributedString(string: "문화예술을 관람하는\n", attributes: [.font: UIFont.sub1, .foregroundColor: UIColor.gray1])
+        attributedString.append(NSAttributedString(string: "주요 목적은 무엇인가요? (택1)", attributes: [.font: UIFont.sub1, .foregroundColor: UIColor.gray1]))
+        $0.attributedText = attributedString
+        $0.numberOfLines = 2
+        $0.textAlignment = .left
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private lazy var selectPurpseView = SelectPurposeView()
+    
+    private let subTitle = UILabel().then {
+        $0.text = "나중에 다시 수정할 수 있어요!"
+        $0.textAlignment = .center
+        $0.textColor = .gray4
+        $0.font = .body4
     }
-    */
+    
+    private lazy var footerView = OnboardingFooterView().then {
+        $0.showBottomBorder = false
+        $0.delegate = self
+        $0.updateNextButtonState(isEnabled: true)
+        $0.nextButtonTitle = "입장하기"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Set ViewController
+    override func setViewController() {
+        view.backgroundColor = .white
+        
+        [headerView, progressView, mainTitle, selectPurpseView, subTitle, footerView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(56)
+        }
+        
+        progressView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(10)
+            $0.height.equalTo(6)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        mainTitle.snp.makeConstraints {
+            $0.top.equalTo(progressView.snp.bottom).offset(28)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        selectPurpseView.snp.makeConstraints {
+            $0.top.equalTo(mainTitle.snp.bottom).offset(32)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(312)
+        }
+        
+        subTitle.snp.makeConstraints {
+            $0.bottom.equalTo(footerView.snp.top).offset(-18)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        footerView.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(75)
+        }
+    }
+    
+    override func bind() {
+        selectPurpseView.inputSelectPurpose
+            .subscribe(with: self, onNext: { owner, type in
+                print(type)
+            })
+            .disposed(by: disposeBag)
+    }
+}
 
+extension RegisterSelectPurposeViewController: OnboardingHeaderViewDelegate {
+    func leftButtonDidTap() {
+        coordinator?.pop()
+    }
+    
+    func rightButtonDidTap() {
+        print("testLog: rightButton Tapped")
+    }
+}
+
+extension RegisterSelectPurposeViewController: OnboardingFooterViewDelegate {
+    func nextButtonDidTap() {
+        print("testLog: nextButton Tapped")
+    }
 }
