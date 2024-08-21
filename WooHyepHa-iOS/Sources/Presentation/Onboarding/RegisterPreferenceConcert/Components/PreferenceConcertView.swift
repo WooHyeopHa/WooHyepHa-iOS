1f3410cc24dbe3008558c05c1fe5b33b9faa4f0e
YeoSeongil <seongil_yeo@naver.com>
2024-08-21 17:32:06 +0900

    [ADD] #12 - PreferenceConcertView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterPreferenceConcert/Components/PreferenceConcertView.swift
@@ -0,0 +1,20 @@
+//
+//  PreferenceConcertView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/21/24.
+//
+
+import UIKit
+
+class PreferenceConcertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
