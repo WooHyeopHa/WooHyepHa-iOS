c9bfadb4427c276fa64b94550ae71f0f1394f1c3
YeoSeongil <seongil_yeo@naver.com>
2024-08-21 10:16:10 +0900

    [ADD] #12 - OnboardingButton 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/Components/OnboardingButton.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingButton.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/21/24.
+//
+
+import UIKit
+
+class OnboardingButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
