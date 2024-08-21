50d8002e196e1aae9b89ec8dffa22279c7fc41e9
YeoSeongil <seongil_yeo@naver.com>
2024-08-21 22:12:52 +0900

    [ADD] #12 - SelectPurposeView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterSelectPurpose/Components/SelectPurposeView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectPurposeView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/21/24.
+//
+
+import UIKit
+
+class SelectPurposeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
