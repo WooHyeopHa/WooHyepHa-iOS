b5b3dc694d6a619c7ff38a8a1990e0d81bfa86cc
YeoSeongil <seongil_yeo@naver.com>
2024-08-21 21:07:27 +0900

    [ADD] #12 - RegisterPreferenceMusicalView 파일 생성

---
+++ WooHyepHa-iOS/Sources/Presentation/Onboarding/RegisterPreferenceMusical/Components/RegisterPreferenceMusicalView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterPreferenceMusicalView.swift
+//  WooHyepHa-iOS
+//
+//  Created by 여성일 on 8/21/24.
+//
+
+import UIKit
+
+class RegisterPreferenceMusicalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
